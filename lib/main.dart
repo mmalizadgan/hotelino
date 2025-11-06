cc0803dc94875892f45a365d5ae6b7d9e88988e7
Mohammad Mahdi Alizadgan <mmalizadgann@gmail.com>
2025-11-06 06:22:16 +0330

    add provider package to dependencies and multiprovider in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:hotelino/core/theme/theme_provider.dart';
 import 'package:hotelino/splash_screen/splashScreen.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(MyApp());
+  //The line below is to check whether Flutter has been able to retrieve the
+  //necessary information from the system or not.
+  final widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (_) => ThemeProvider(
+            WidgetsBinding.instance.platformDispatcher.platformBrightness,
+          ),
+        ),
+      ],
+      child: MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
