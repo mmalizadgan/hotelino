b0d05070875ee1675b12ecdd20bbd04379477d08
Mohammad Mahdi Alizadgan <mmalizadgann@gmail.com>
2025-11-08 00:14:06 +0330

    bug fixed : i have two MatrialApp widget and theme not working

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:hotelino/core/theme/app_theme.dart';
 import 'package:hotelino/core/theme/theme_provider.dart';
 import 'package:hotelino/splash_screen/splashScreen.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:provider/provider.dart';
 
 void main() {
-  //The line below is to check whether Flutter has been able to retrieve the
-  //necessary information from the system or not.
-  final widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  WidgetsFlutterBinding.ensureInitialized();
 
   runApp(
-    MultiProvider(
-      providers: [
-        ChangeNotifierProvider(
-          create: (_) => ThemeProvider(
-            WidgetsBinding.instance.platformDispatcher.platformBrightness,
-          ),
-        ),
-      ],
-      child: MyApp(),
+    ChangeNotifierProvider(
+      create: (_) => ThemeProvider(
+        WidgetsBinding.instance.platformDispatcher.platformBrightness,
+      ),
+      child: const MyApp(),
     ),
   );
 }
@@ -32,11 +27,20 @@ class MyApp extends StatelessWidget {
       designSize: const Size(430, 932),
       minTextAdapt: true,
       splitScreenMode: true,
-      child: MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'Hotelino',
-        theme: ThemeData(primarySwatch: Colors.blue),
-        home: SplashPage(),
+      child: Consumer<ThemeProvider>(
+        builder: (context, themeProvider, child) {
+          return MaterialApp(
+            debugShowCheckedModeBanner: false,
+            title: 'Hotelino',
+            theme: AppTheme.lightTheme,
+            darkTheme: AppTheme.darkTheme,
+            themeMode: themeProvider.brightness == Brightness.dark
+                ? ThemeMode.dark
+                : ThemeMode.light,
+            home:
+                const SplashPage(), // بعد از SplashPage می‌تونی MyHomePage بذاری
+          );
+        },
       ),
     );
   }
--- lib/myHomePage.dart
+++ lib/myHomePage.dart
@@ -1,59 +1,34 @@
 import 'package:flutter/material.dart';
-import 'package:hotelino/core/theme/app_theme.dart';
 import 'package:hotelino/core/theme/theme_provider.dart';
 import 'package:provider/provider.dart';
 
-class MyHomePage extends StatefulWidget {
+class MyHomePage extends StatelessWidget {
   const MyHomePage({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> with WidgetsBindingObserver {
-  @override
-  void initState() {
-    super.initState();
-    WidgetsBinding.instance.addObserver(this);
-  }
-
-  @override
-  void dispose() {
-    WidgetsBinding.instance.removeObserver(this);
-    super.dispose();
-  }
-
-  @override
-  void didChangePlatformBrightness() {
-    super.didChangePlatformBrightness();
-
-    final brightness =
-        WidgetsBinding.instance.platformDispatcher.platformBrightness;
-    Provider.of<ThemeProvider>(
-      context,
-      listen: false,
-    ).updateBrightness(brightness);
-  }
-
-  @override
   Widget build(BuildContext context) {
-    return Consumer<ThemeProvider>(
-      builder: (context, themeModeProvider, child) {
-        return MaterialApp(
-          debugShowCheckedModeBanner: false,
-          theme: themeModeProvider.brightness == Brightness.light
-              ? AppTheme.lightTheme
-              : AppTheme.darkTheme,
-          home: Center(
-            child: ElevatedButton(
-              onPressed: () {
-                themeModeProvider.toggleTheme();
-              },
-              child: Text("Change Theme"),
-            ),
+    final themeProvider = Provider.of<ThemeProvider>(context);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Home Page"),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.brightness_6),
+            onPressed: () {
+              themeProvider.toggleTheme();
+            },
           ),
-        );
-      },
+        ],
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            themeProvider.toggleTheme();
+          },
+          child: const Text("Change Theme"),
+        ),
+      ),
     );
   }
 }
