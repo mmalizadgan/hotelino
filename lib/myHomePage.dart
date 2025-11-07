31bde92510e55b031f7bf34eadc479eb5e1f2902
Mohammad Mahdi Alizadgan <mmalizadgann@gmail.com>
2025-11-07 23:33:26 +0330

    add provider in my home page

--- lib/myHomePage.dart
+++ lib/myHomePage.dart
@@ -1,10 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:hotelino/core/theme/app_theme.dart';
+import 'package:hotelino/core/theme/theme_provider.dart';
+import 'package:provider/provider.dart';
 
 class MyHomePage extends StatelessWidget {
   const MyHomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Consumer<ThemeProvider>(
+      builder: (context, themeModeProvider, child) {
+        return MaterialApp(
+          debugShowCheckedModeBanner: false,
+          theme: themeModeProvider.brightness == Brightness.light
+              ? AppTheme.lightTheme
+              : AppTheme.darkTheme,
+          home: Center(
+            child: ElevatedButton(
+              onPressed: () {
+                themeModeProvider.toggleTheme();
+              },
+              child: Text("Change Theme"),
+            ),
+          ),
+        );
+      },
+    );
   }
 }
