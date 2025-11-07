c66dae42249775177e25a5439dca4221449ed53e
Mohammad Mahdi Alizadgan <mmalizadgann@gmail.com>
2025-11-07 23:55:56 +0330

    add system icon bar for under 12 android

--- lib/core/theme/theme_provider.dart
+++ lib/core/theme/theme_provider.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class ThemeProvider extends ChangeNotifier {
   Brightness _brightness;
@@ -14,6 +15,21 @@ class ThemeProvider extends ChangeNotifier {
 
   void updateBrightness(Brightness brightness) {
     _brightness = brightness;
+    _updateSystemUI();
     notifyListeners();
+  }
+
+  void _updateSystemUI() {
+    bool isDark = _brightness == Brightness.dark;
+    SystemChrome.setSystemUIOverlayStyle(
+      SystemUiOverlayStyle(
+        statusBarColor: Colors.transparent,
+        statusBarIconBrightness: isDark ? Brightness.light : Brightness.dark,
+        systemNavigationBarColor: isDark ? Colors.black : Colors.white,
+        systemNavigationBarIconBrightness: isDark
+            ? Brightness.light
+            : Brightness.dark,
+      ),
+    );
   }
 }
