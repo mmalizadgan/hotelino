cba0053e8070e1310993ad93e609b148f98e6c02
Mohammad Mahdi Alizadgan <mmalizadgann@gmail.com>
2025-11-09 20:14:59 +0330

    add onboarding provider

---
+++ lib/features/onboardin/presentation/onboarding_provider.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:hotelino/features/onboardin/data/repositories/onboarding_repository.dart';
+
+class OnboardingProvider extends ChangeNotifier {
+  final OnboardingRepository repository;
+
+  OnboardingProvider(this.repository) {
+    _loadData();
+  }
+
+  int currentPage = 0;
+
+  List<Map<String, String>> onboardingData = [];
+
+  void _loadData() {
+    onboardingData = repository.onboardingData;
+    notifyListeners();
+  }
+
+  void updateCurrentPage(int index) {
+    currentPage = index;
+    notifyListeners();
+  }
+}
