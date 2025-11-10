1887f19601a47dd41dd985d3d43c08f3687a829c
Mohammad Mahdi Alizadgan <mmalizadgann@gmail.com>
2025-11-10 20:56:23 +0330

    finish onboarding pages

--- lib/features/onboardin/presentation/onboarding_page.dart
+++ lib/features/onboardin/presentation/onboarding_page.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:hotelino/features/onboardin/presentation/onboarding_provider.dart';
+import 'package:hotelino/features/onboardin/presentation/widget/onboarding_button.dart';
 import 'package:hotelino/features/onboardin/presentation/widget/onboarding_indicator.dart';
 import 'package:hotelino/features/onboardin/presentation/widget/onboarding_item.dart';
+import 'package:hotelino/routes/app_route.dart';
 import 'package:provider/provider.dart';
 
 class OnboardingPage extends StatefulWidget {
@@ -20,6 +22,7 @@ class _OnboardingPageState extends State<OnboardingPage> {
     final onboardingProvider = Provider.of<OnboardingProvider>(context);
     final onboardingData = onboardingProvider.onboardingData;
     final totalPage = onboardingData.length;
+    final theme = Theme.of(context);
     return Scaffold(
       body: Column(
         children: [
@@ -39,13 +42,95 @@ class _OnboardingPageState extends State<OnboardingPage> {
             ),
           ),
           SizedBox(height: 20.w),
-          OnboardingIndicator(
-            currentIndex: onboardingProvider.currentPage,
-            totalPages: totalPage,
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 16.w),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                OnboardingButton(
+                  visible: onboardingProvider.currentPage > 0,
+                  icon: Icons.arrow_back,
+                  onPressed: () => _previosPage(),
+                  backgraoundColor: Colors.transparent,
+                  iconColor: theme.colorScheme.primary,
+                ),
+                OnboardingIndicator(
+                  currentIndex: onboardingProvider.currentPage,
+                  totalPages: totalPage,
+                ),
+                OnboardingButton(
+                  visible: onboardingProvider.currentPage < totalPage - 1,
+                  icon: Icons.arrow_forward,
+                  onPressed: () => _nextPage(),
+                  backgraoundColor: theme.colorScheme.primary,
+                  iconColor: Colors.white,
+                ),
+              ],
+            ),
           ),
-          SizedBox(height: 159.w),
+          SizedBox(height: 53.h),
+          if (totalPage > 1) ...[
+            AnimatedSwitcher(
+              duration: Duration(microseconds: 300),
+              transitionBuilder: (child, animation) {
+                return SizeTransition(
+                  child: child,
+                  sizeFactor: animation,
+                  axisAlignment: -1,
+                );
+              },
+              child: onboardingProvider.currentPage == totalPage - 1
+                  ? Padding(
+                      padding: EdgeInsets.symmetric(
+                        horizontal: 16.w,
+                        vertical: 40.h,
+                      ),
+                      child: SizedBox(
+                        width: 398.w,
+                        height: 50.h,
+                        child: ElevatedButton(
+                          onPressed: () {
+                            Navigator.pushReplacementNamed(
+                              context,
+                              AppRoute.myHomePage,
+                            );
+                          },
+                          child: const Text("شروع رزرو هتل ها"),
+                        ),
+                      ),
+                    )
+                  : null,
+            ),
+          ],
         ],
       ),
     );
+  }
+
+  void _previosPage() {
+    final onboardingProvider = Provider.of<OnboardingProvider>(
+      context,
+      listen: false,
+    );
+    if (onboardingProvider.currentPage > 0) {
+      _pageController.previousPage(
+        duration: Duration(milliseconds: 500),
+        curve: Curves.ease,
+      );
+    }
+  }
+
+  void _nextPage() {
+    final onboardingProvider = Provider.of<OnboardingProvider>(
+      context,
+      listen: false,
+    );
+    if (onboardingProvider.currentPage <
+        onboardingProvider.onboardingData.length - 1) {
+      _pageController.nextPage(
+        duration: Duration(milliseconds: 500),
+        curve: Curves.ease,
+      );
+    }
   }
 }
---
+++ lib/features/onboardin/presentation/widget/onboarding_button.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class OnboardingButton extends StatelessWidget {
+  final bool visible;
+  final IconData icon;
+  final VoidCallback onPressed;
+  final Color backgraoundColor;
+  final Color iconColor;
+
+  const OnboardingButton({
+    super.key,
+    required this.visible,
+    required this.icon,
+    required this.onPressed,
+    required this.backgraoundColor,
+    required this.iconColor,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedSwitcher(
+      duration: const Duration(milliseconds: 300),
+      transitionBuilder: (child, animation) {
+        return FadeTransition(opacity: animation, child: child);
+      },
+      child: visible
+          ? Container(
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                border: Border.all(
+                  color: Theme.of(context).colorScheme.primary,
+                  width: 1.w,
+                ),
+              ),
+              child: FloatingActionButton(
+                onPressed: onPressed,
+                elevation: 0,
+                backgroundColor: backgraoundColor,
+                shape: const CircleBorder(),
+                child: Icon(icon, color: iconColor),
+              ),
+            )
+          : SizedBox(),
+    );
+  }
+}
