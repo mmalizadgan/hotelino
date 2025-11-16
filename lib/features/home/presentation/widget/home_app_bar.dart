3f447aed41c72e0edadaf5c3b0d34cddc32b7908
Mohammad Mahdi Alizadgan <mmalizadgann@gmail.com>
2025-11-16 21:24:53 +0330

    finish home app bar

--- lib/features/home/presentation/widget/home_app_bar.dart
+++ lib/features/home/presentation/widget/home_app_bar.dart
@@ -1,11 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:hotelino/core/theme/theme_provider.dart';
+import 'package:hotelino/features/home/presentation/provider/profile_provider.dart';
+import 'package:provider/provider.dart';
 
 class HomeAppBar extends StatelessWidget implements PreferredSizeWidget {
   const HomeAppBar({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final themeProvider = Provider.of<ThemeProvider>(context);
+
     return AppBar(
       elevation: 0,
       automaticallyImplyLeading: false,
@@ -20,19 +25,57 @@ class HomeAppBar extends StatelessWidget implements PreferredSizeWidget {
                     onPressed: () {},
                     icon: Icon(Icons.notifications_none),
                   ),
+
+                  Consumer<ProfileProvider>(
+                    builder: (context, profileProvider, child) {
+                      return profileProvider.profile?.notifications != null &&
+                              profileProvider.profile!.notifications > 0
+                          ? Positioned(
+                              right: 12,
+                              top: 14,
+                              child: CircleAvatar(
+                                radius: 4,
+                                backgroundColor: Theme.of(
+                                  context,
+                                ).colorScheme.primary,
+                              ),
+                            )
+                          : SizedBox();
+                    },
+                  ),
                 ],
+              ),
+              IconButton(
+                onPressed: () {
+                  themeProvider.toggleTheme();
+                },
+                icon: Icon(
+                  themeProvider.brightness == Brightness.dark
+                      ? Icons.light_mode
+                      : Icons.dark_mode,
+                ),
               ),
             ],
           ),
-          Row(
-            children: [
-              Text("کاربر", style: Theme.of(context).textTheme.titleMedium),
-              SizedBox(width: 8.w),
-              CircleAvatar(
-                radius: 20,
-                backgroundImage: NetworkImage("https://picsum.photos/200/300"),
-              ),
-            ],
+          Consumer<ProfileProvider>(
+            builder: (context, profileProvider, child) {
+              return Row(
+                children: [
+                  Text(
+                    profileProvider.profile?.name ?? "کاربر",
+                    style: Theme.of(context).textTheme.titleMedium,
+                  ),
+                  SizedBox(width: 8.w),
+                  CircleAvatar(
+                    radius: 20,
+                    backgroundImage: NetworkImage(
+                      profileProvider.profile?.avatarUrl ??
+                          "https://picsum.photos/200/300",
+                    ),
+                  ),
+                ],
+              );
+            },
           ),
         ],
       ),
