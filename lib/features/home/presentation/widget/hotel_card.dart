4aa1cd3335608a0b779ae645f7dcc94ec17405aa
Mohammad Mahdi Alizadgan <mmalizadgann@gmail.com>
2025-11-21 00:14:11 +0330

    add hotel card

--- lib/features/home/presentation/widget/hotel_card.dart
+++ lib/features/home/presentation/widget/hotel_card.dart
@@ -3,7 +3,6 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:hotelino/core/utils/price_formater.dart';
 import 'package:hotelino/features/home/data/models/hotels.dart';
 import 'package:hotelino/features/home/presentation/provider/favorite_item_provider.dart';
-import 'package:hotelino/routes/test.dart';
 import 'package:provider/provider.dart';
 
 class HotelCard extends StatelessWidget {
@@ -32,13 +31,58 @@ class HotelCard extends StatelessWidget {
                   Row(
                     children: [
                       SizedBox(width: 12.w),
-                      Icon(Icons.star, size: 20, color: Colors.amber),
+                      Icon(Icons.star, size: 15, color: Colors.amber),
                       SizedBox(width: 2.w),
                       Text(
                         "${hotel.rating} (${priceFormater(hotel.reviewCount)})",
+                        style: Theme.of(context).textTheme.titleSmall,
                       ),
+                      Spacer(),
+                      Text(
+                        hotel.name,
+                        style: Theme.of(context).textTheme.titleLarge,
+                      ),
+                      SizedBox(width: 8.w),
                     ],
                   ),
+                  SizedBox(height: 4.h),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      SizedBox(width: 8.w),
+                      Text(
+                        "${hotel.city}, ${hotel.country}",
+                        style: TextStyle(color: Colors.grey),
+                      ),
+                      SizedBox(width: 2.w),
+                      Icon(Icons.location_on, color: Colors.grey, size: 16),
+                      SizedBox(width: 8.w),
+                    ],
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(right: 8.w),
+                    child: Text(
+                      " شب از  ${priceFormater(hotel.pricePerNight)}",
+                      style: Theme.of(context).textTheme.titleLarge,
+                    ),
+                  ),
+                  SizedBox(height: 8.h),
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 8.w),
+                    child: SizedBox(
+                      width: 216.w,
+                      height: 30.h,
+                      child: ElevatedButton(
+                        onPressed: () {},
+                        child: Text(
+                          "مشاهده و انتخاب اتاق",
+                          style: Theme.of(context).textTheme.labelMedium
+                              ?.copyWith(color: Colors.white),
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 10.h),
                 ],
               ),
             ),
