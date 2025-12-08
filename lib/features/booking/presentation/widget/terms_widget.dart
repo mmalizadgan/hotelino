e51eb7634b105bb2c57af8c36dca47c717c23d3a
Mohammad Mahdi Alizadgan <mmalizadgann@gmail.com>
2025-12-08 23:55:44 +0330

    add policy for hotelino project

--- lib/features/booking/presentation/widget/terms_widget.dart
+++ lib/features/booking/presentation/widget/terms_widget.dart
@@ -16,7 +16,9 @@ class _TermsWidgetState extends State<TermsWidget> {
       mainAxisAlignment: MainAxisAlignment.end,
       children: [
         GestureDetector(
-          onTap: () {},
+          onTap: () {
+            _showTermsDialog(context);
+          },
           child: RichText(
             text: TextSpan(
               style: Theme.of(
@@ -48,6 +50,34 @@ class _TermsWidgetState extends State<TermsWidget> {
           },
         ),
       ],
+    );
+  }
+
+  void _showTermsDialog(BuildContext context) {
+    showModalBottomSheet(
+      context: context,
+      builder: (context) {
+        return Padding(
+          padding: EdgeInsets.all(24),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              Text(
+                "قوانین برنامه هتلینو",
+                style: Theme.of(context).textTheme.displayMedium,
+              ),
+              Text(
+                "هتلینو یکی از برنامه‌های معتبر از دوره‌ی یاقوت فلاتر است که توسط پیج دانیجت و با تدریس امیرحسین محمدی طراحی و توسعه یافته است. این برنامه به شما این امکان را می‌دهد تا به راحتی هتل‌های مختلف را در کشورهای گوناگون جستجو کرده و رزرو کنید\n\n"
+                "شما قادر خواهید بود پروفایل شخصی خود را ایجاد کنید و هتل‌هایی که به نیازهای شما نزدیک‌تر هستند را پیدا کنید. توجه داشته باشید که هتل‌های رزرو شده قابل لغو نبوده و پس از انجام رزرو، تغییرات در این زمینه امکان‌پذیر نمی‌باشد\n\n"
+                "قیمت‌های هتل‌ها به صورت مقطوع اعلام شده‌اند و هیچ‌گونه تغییر قیمتی پس از رزرو نخواهید داشت. همچنین، اطلاعات هتل‌ها به دقت بررسی و به روزرسانی می‌شوند تا شما تجربه‌ای رضایت‌بخش از اقامت خود داشته باشید",
+                textDirection: TextDirection.rtl,
+                textAlign: TextAlign.justify,
+                style: Theme.of(context).textTheme.bodyMedium,
+              ),
+            ],
+          ),
+        );
+      },
     );
   }
 }
