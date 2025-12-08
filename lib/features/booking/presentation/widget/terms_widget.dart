1fb7f67dffabd126e7bdef41e1e91dc1f087be38
Mohammad Mahdi Alizadgan <mmalizadgann@gmail.com>
2025-12-08 23:44:37 +0330

    edite term widget

--- lib/features/booking/presentation/widget/terms_widget.dart
+++ lib/features/booking/presentation/widget/terms_widget.dart
@@ -15,8 +15,31 @@ class _TermsWidgetState extends State<TermsWidget> {
     return Row(
       mainAxisAlignment: MainAxisAlignment.end,
       children: [
-        GestureDetector(onTap: () {}, child: Text("")),
+        GestureDetector(
+          onTap: () {},
+          child: RichText(
+            text: TextSpan(
+              style: Theme.of(
+                context,
+              ).textTheme.bodyMedium?.copyWith(color: Colors.grey.shade700),
+              children: [
+                const TextSpan(text: "قوانین برنامه"),
+                TextSpan(
+                  text: "هتلینو",
+                  style: TextStyle(
+                    fontWeight: FontWeight.w600,
+                    color: Theme.of(context).colorScheme.primary,
+                  ),
+                ),
+                const TextSpan(text: "را خوانده و آنها را میپزیم."),
+              ],
+            ),
+            textDirection: TextDirection.rtl,
+          ),
+        ),
         Checkbox(
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(4)),
+          activeColor: Theme.of(context).colorScheme.primary,
           value: isChecked,
           onChanged: (value) {
             setState(() {
