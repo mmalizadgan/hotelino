a09239101db7ad513a3c5e1a382b004342c9ea70
Mohammad Mahdi Alizadgan <mmalizadgann@gmail.com>
2025-12-09 23:39:13 +0330

    fix some bug

--- lib/features/booking/presentation/widget/terms_widget.dart
+++ lib/features/booking/presentation/widget/terms_widget.dart
@@ -19,12 +19,18 @@ class TermsWidget extends StatefulWidget {
 }
 
 class _TermsWidgetState extends State<TermsWidget> {
-  bool isChecked = false;
+  late bool isChecked;
 
   resetChecked() {
     setState(() {
       isChecked = false;
     });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    isChecked = widget.initialValue;
   }
 
   @override
@@ -34,6 +40,11 @@ class _TermsWidgetState extends State<TermsWidget> {
       onSaved: widget.onSaved,
       validator: widget.validator,
       builder: (FormFieldState<bool> field) {
+        //sync field
+        if (field.value != isChecked) {
+          field.didChange(isChecked);
+        }
+
         return Column(
           crossAxisAlignment: CrossAxisAlignment.end,
           children: [
@@ -84,6 +95,7 @@ class _TermsWidgetState extends State<TermsWidget> {
                   onChanged: (value) {
                     setState(() {
                       isChecked = value ?? false;
+                      field.didChange(isChecked);
                     });
                   },
                 ),
